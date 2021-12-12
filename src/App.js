f6abb8f0204dae1046c9cbbe72bcd025dd4ae963
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-12-12 15:19:34 +0530

    remote import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,21 @@
 import "./styles.css";
+import * as THREE from "three";
+import { useState, useRef, Suspence, useMemo } from "react";
+import {
+  CanvasGradient,
+  useThree,
+  useFrame,
+  useLoader
+} from "@react-three/fiber";
+import {
+  Reflector,
+  CameraShake,
+  OrbitControls,
+  useTexture
+} from "@react-three/drei";
+import { KernelSize } from "postprocessing";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 
 export default function App() {
   return (
