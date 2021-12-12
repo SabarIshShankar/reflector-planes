68723865ce3d5a563bfdea1424f0d92821a22533
SabarIshShankar <66781581+SabarIshShankar@users.noreply.github.com>
2021-12-12 18:28:02 +0530

    triangle

--- src/App.js
+++ src/App.js
@@ -17,6 +17,34 @@ import { KernelSize } from "postprocessing";
 import { EffectComposer, Bloom } from "@react-three/postprocessing";
 import { SVGLoader } from "three/examples/jsm/loaders/SVGLoader";
 
+function Triangle({ color, ...props }) {
+  const ref = useRef();
+  const [r] = useState(() => Math.random() * 10000);
+  useFrame(
+    (_) =>
+      (ref.current.position.y = -1.75 + Math.sin(_.clock.elapsedTime + r) / 10)
+  );
+  const {
+    paths: [path]
+  } = useLoader(SVGLoader, "./triangle.svg");
+  const geom = useMemo(
+    () =>
+      SVGLoader.pointsToStroke(
+        path.subPaths[0].getPoints(),
+        path.userData.style
+      ),
+    []
+  );
+  return (
+    <group ref={ref}>
+      <mesh geometry={geom} {...props}>
+        <meshBasicMaterial color={color} toneMapped={false} />{" "}
+      </mesh>
+    </group>
+  );
+}
+
+function Rig({ children }) {}
 export default function App() {
   return (
     <div className="App">
